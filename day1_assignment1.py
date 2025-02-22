8f74fb8523c583123410baae5978be230c59bc9b
JBBansig <jaybee.bansig@aclcbutuan.edu.ph>
2025-02-22 10:59:12 +0800

    first commit

---
+++ day1_assignment1.py
@@ -0,0 +1,8 @@
+# Simple Python program to greet the user
+
+# Ask for user input
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+
+# Print a greeting message
+print(f"Hello, {name}! You are {age} years old.")
